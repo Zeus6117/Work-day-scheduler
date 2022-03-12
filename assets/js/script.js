f6c9e1dfed42a27e181978d3b488f63f37b6c257
Jordan King <96736562+Zeus6117@users.noreply.github.com>
2022-03-12 13:42:35 -0500

    added more functions/ filled in older ones

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,17 @@ function createEvents(eventText) {
 }
 
 function loadEvents() {
-
+ //setting variable count equal to 8
+ for (let count = 8; count < 19; count++) {
+    //getting the count number from localStorage
+    localStorage.getItem(count);
+    //setting countString equal to the getItem(count)
+    let countString = localStorage.getItem(count);
+    if (countString) {
+      //the ID for that count is pulled and it will place the value of countString
+      $("#" + count).val(countString);
+    }
+  }
 }
 
 //save events to localStorage
@@ -17,12 +27,39 @@ function saveEvents(clickedButton) {
     events[$(clickedButton).parent().attr("data-hour")] = $("data-hour").val();
   }
 
-//clock element
-var clock = document.getElementById('currentDay');
-
+//set up time/clock element
+var time = document.getElementById('currentDay');
 function currentDay() {
     time.textContent = new Date().toString();
 }
 
 //refresh every second
 setInterval(currentDay, 1000)
+
+//use moment.js to add the text content of the current month, day, year
+function checkEvent() {
+    var clock = moment().hours();
+  
+    //set up the event for all time blocks
+    $(".event-category").each(function () {
+      var timeBlock = parseInt($(this).attr("data-hour"));
+  
+      //if conditional for if timeBlock is less than the clock, will add past class
+      if (timeBlock < clock) {
+        $(this).addClass("past");
+  
+        //else if conditional for if timeBlock is equal to the clock, will add present class & remove past class
+      } else if (timeBlock === clock) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+  
+        //else if conditional for if timeBlock is equal to the clock, will add future class & remove either past or present
+      } else if (timeBlock > clock) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  setInterval(checkEvent, 1000);
